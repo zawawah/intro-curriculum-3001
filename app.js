fedcd8c27bde36f7d4d897b083a1e340208430f3
Soichiro-Yoshimura <soichiro_yoshimura@dwango.co.jp>
2015-12-04 18:34:57 +0900

    format code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
 	'use strict';
     /**
      * 与えられた自然数の階乗を返す
@@ -11,10 +11,10 @@
 		// TODO このコメントを消して正しく実装してください。
 		return result;
 	}
-	
+
 	let assert = require('assert');
-	assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1)}でした`);
-	assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2)}でした`);
-	assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3)}でした`);
-	assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10)}でした`);
+	assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
+	assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
+	assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
+	assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
 })();
