09c4283720d1856e1789b24c6f1be606c3793036
Soichiro-Yoshimura <soichiro_yoshimura@dwango.co.jp>
2015-12-04 18:34:17 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+(function(){
+	'use strict';
+    /**
+     * 与えられた自然数の階乗を返す
+	 * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
+     * @param {Number} n
+     * @returns {Number}
+     */
+	function factorial(n) {
+		let result = 1;
+		// TODO このコメントを消して正しく実装してください。
+		return result;
+	}
+	
+	let assert = require('assert');
+	assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1)}でした`);
+	assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2)}でした`);
+	assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3)}でした`);
+	assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10)}でした`);
+})();
