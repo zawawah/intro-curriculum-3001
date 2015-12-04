67c5503914e0af2f1f2592ccb5e23db00468bafe
Soichiro-Yoshimura <soichiro_yoshimura@dwango.co.jp>
2015-12-04 18:45:33 +0900

    テスト通過後のメッセージを追記

--- app.js
+++ app.js
@@ -17,4 +17,5 @@
 	assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
 	assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
 	assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
+	console.log('すべてのテストを通過しました');
 })();
