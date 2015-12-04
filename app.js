b24ef11eb95acac4768844b6f7b86a234e729c05
Soichiro-Yoshimura <soichiro_yoshimura@dwango.co.jp>
2015-12-04 19:00:16 +0900

    コードの整形

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 (function () {
 	'use strict';
-    /**
-     * 与えられた自然数の階乗を返す
+	/**
+	 * 与えられた自然数の階乗を返す
 	 * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
-     * @param {Number} n
-     * @returns {Number}
-     */
+	 * @param {Number} n
+	 * @returns {Number}
+	 */
 	function factorial(n) {
 		let result = 1;
 		// TODO このコメントを消して正しく実装してください。
