b17db01d458c271306c5883090cfde3c0f274cfc
DDRAGON <davide.0829@gmail.com>
2016-06-03 14:05:15 +0900

    let → const

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
 		return result;
 	}
 
-	let assert = require('assert');
+	const assert = require('assert');
 	assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
 	assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
 	assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
